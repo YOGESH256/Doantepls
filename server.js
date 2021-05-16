fa75ecbed74cea2909985c89e3d3daefdaec9473
Yogesh <2019yogesh.khatri@ves.ac.in>
2021-05-16 17:59:51 +0530

    Project completed

--- server.js
+++ server.js
@@ -58,6 +58,7 @@ app.post("/api/payment/order", (req, res) => {
   instance.orders
     .create(params)
     .then((data) => {
+      console.log(data);
       res.send({ sub: data, status: "success" });
     })
     .catch((error) => {
